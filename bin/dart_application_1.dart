6921fb8b16a5899ff75fe74bf0bb6d8ad3120b68
Ysmailov Nursultan <nursysmailovv@gmail.com>
2024-02-02 17:08:44 +0600

    second commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,46 +1,42 @@
-
-
-
 import 'dart:io';
 
 void main() {
-  print("Введите первое число");
+  print("Вве-дите первое число");
   num num1 = double.tryParse(stdin.readLineSync() ?? "") ?? -12312312321311;
-  if(num1==-12312312321311){
+  if (num1 == -12312312321311) {
     print("Вы ввели не число,введите еще раз!");
     num1 = double.tryParse(stdin.readLineSync() ?? "") ?? -12312312321311;
-  }else{
-    
-  }
-  
+  } else {}
+
   print('Введите операцию (+ - * /)');
   String action = stdin.readLineSync() ?? "";
 
   print("Введите второе число");
   num num2 = double.tryParse(stdin.readLineSync() ?? "") ?? -12312312321311;
-  if(num2==-12312312321311){
+  if (num2 == -12312312321311) {
     print("Вы ввели не число,введите еще раз!");
     num2 = double.tryParse(stdin.readLineSync() ?? "") ?? -12312312321311;
-  }else{
-    
-  }
+  } else {}
 
-  if(num1!=-12312312321311 && num2!=-12312312321311){
-      if(action=='+'){
-        print(num1+num2);
-      }else if(action=='-'){
-        print(num1-num2);
-      }else if(action=='/'){
-        print(num1/num2);
-      }else if(action=='*'){
-        print(num1*num2);
-      }else{
-        print("Вы ввели некорректную операцию");
-      }
-    }else{
-      print("Одно из введенных значений не число");
+  var result;
+
+  if (num1 != -12312312321311 && num2 != -12312312321311) {
+    if (action == '+') {
+      result = num1 + num2;
+    } else if (action == '-') {
+      result = num1 - num2;
+    } else if (action == '/') {
+      result = num1 - num2;
+    } else if (action == '*') {
+      result = num1 * num2;
+    } else {
+      print("Вы ввели некорректную операцию");
     }
-  // print("Введите второе число");
 
-
+    if (result is double) {
+      result % 1 == 0 ? print(result.toInt()) : print(result);
+    }
+  } else {
+    print("Одно из введенных значений не число");
+  }
 }
